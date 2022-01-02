77e5acc32fb0fddefdd1e07e521e3a542ce42211
Tomás Cândido <2201142@my.ipleiria.pt>
2022-01-02 16:24:48 +0000

    Volley

--- app/src/main/java/com/example/projetomobile/InicialFragment.java
+++ app/src/main/java/com/example/projetomobile/InicialFragment.java
@@ -90,12 +90,6 @@ public class InicialFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
-
-
-
-
     }
 
     @Override
--- app/src/main/java/com/example/projetomobile/LoginActivity.java
+++ app/src/main/java/com/example/projetomobile/LoginActivity.java
@@ -5,8 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.projetomobile.databinding.ActivityLoginBinding;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,11 +30,46 @@ public class LoginActivity extends AppCompatActivity {
         View view=binding.getRoot();
         setContentView(view);
 
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url ="http://10.0.2.2/projetoweb/backend/web/index.php?r=api%2Fuser";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            for (int i=0;i<response.length();i++){
+                                JSONObject jsonObject= response.getJSONObject(i);
+                                String username= jsonObject.getString("username");
+                                Toast.makeText(LoginActivity.this,username,Toast.LENGTH_LONG).show();
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+        queue.add(jsonArrayRequest);
+
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
+
+
+
             }
         });
     }
