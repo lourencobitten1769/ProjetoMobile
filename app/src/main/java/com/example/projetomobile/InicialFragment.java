ea65e5ceb57460804bbfed7d5912140636c5e30c
Tomás Cândido <2201142@my.ipleiria.pt>
2022-01-24 13:29:18 +0000

    Alterações

--- app/src/main/java/com/example/projetomobile/InicialFragment.java
+++ app/src/main/java/com/example/projetomobile/InicialFragment.java
@@ -2,10 +2,12 @@ package com.example.projetomobile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -38,6 +40,7 @@ public class InicialFragment extends Fragment implements AdapterTypeHoriz.OnItem
     private AdapterProdutos adapterProdutos;
     private RecyclerView.LayoutManager layoutManager;
     List<Product> products=new ArrayList<>();
+    EditText edtProcurar;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -98,7 +101,7 @@ public class InicialFragment extends Fragment implements AdapterTypeHoriz.OnItem
 
         horizontal_recycler=view.findViewById(R.id.horizontal_recycler);
 
-
+        edtProcurar=view.findViewById(R.id.edtProcurar);
 
 
         dbHelper= new DBHelper(getActivity());
@@ -227,6 +230,9 @@ public class InicialFragment extends Fragment implements AdapterTypeHoriz.OnItem
         intent.putExtras(bundleProduct);
         startActivity(intent);
     }
+
+
+
 }
 
 
